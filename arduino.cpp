958dcf013a4b8b8147575e85f224833e8c085f33
matthes <matthesboettcher03@gmail.com>
2024-11-20 22:52:16 +0100

    erster Ablauf zum pruefen per CLK

--- arduino.cpp
+++ arduino.cpp
@@ -1,129 +1,67 @@
+//
+// Created by Matthes Böttcher on 18.11.24.
+//
+#include <iostream>
+
+#include "arduino.h"
+
+void start();
+
 void setup() {
     pinMode(2, INPUT);
-    pinMode(3, INPUT);
+    pinMode(3, OUTPUT); // wird zu PS - senden? Also OUTPUT
     pinMode(4, INPUT);
     pinMode(5, INPUT);
+
     pinMode(6, INPUT);
-    pinMode(7, INPUT);
+    pinMode(7, OUTPUT); // wird zu PS - senden? Also OUTPUT
     pinMode(8, INPUT);
     pinMode(9, INPUT);
-    pinMode(10, INPUT);
     Serial.begin(9600);
 }
 
-void checkStringOnSpecialCharacters();
-
-string testText = "eins 1111B11  Bbb";
 int alterWert = -1;
 int neuerWert;
 int counter = 1;
 
+
 void loop() {
-    int s2 = digitalRead(2);
-    int s3 = digitalRead(3);
-    int s4 = digitalRead(4);
-    int s5 = digitalRead(5);
-    int s6 = digitalRead(6);
-    int s7 = digitalRead(7);
-    int s8 = digitalRead(8);
-    int s9 = digitalRead(9);
+    int CLK = digitalRead(2); // CLK
+    //int p3 = digitalRead(3); // ist output für PS
+    int p4 = digitalRead(4);
+    int p5 = digitalRead(5);
 
-    neuerWert = (1<<s6) | (2<<s7) | (3<<s8) | (4<<s9); // die letzten Pins einlesen und per Binär in int umwandeln
+    int p6 = digitalRead(6);
+    //int p7 = digitalRead(7); // ist output für PS
+    int p8 = digitalRead(8);
+    int p9 = digitalRead(9);
 
-    // wird nur ausgeführt falls sich der Wert geändert hat (nicht der selbe eingelesen wurde)
-    if (neuerWert != alterWert) {
+    // die letzten Pins einlesen und per Binär in int umwandeln
+    neuerWert = (p4 << 1) | (p5);
+
+    if (alterWert != neuerWert) {
         alterWert = neuerWert;
-        Serial.print("Counter: ");
-        Serial.print(counter);
-        Serial.print(" - ");
 
-        Serial.print(s2);
-        Serial.print(s3);
-        Serial.print(s4);
-        Serial.print(s5);
-
-        Serial.print(" ");
-
-        Serial.print(s6);
-        Serial.print(s7);
-        Serial.print(s8);
-        Serial.print(s9);
-
-        Serial.println();
-        counter++;
+        start();
     }
-
-    checkStringOnSpecialCharacters();
 }
 
-void checkStringOnSpecialCharacters() {
-    bool einsDavor = false;
+void start() { // wird dauerhaft aufgerufen
+    if (CLK == 1) { // hier werden die eigentlichen Werte gesendet
 
-    for (char ch : testText) {
-        switch (ch) {
-            case '1':
-                if (einsDavor) {
-                    writeValuesToOtherB15('1');
-                    //cout << "1";
-                }
-                einsDavor = true;
-                break;
-            case 'B':
-                if (!einsDavor) {
-                    writeValuesToOtherB15('B');
-                    //cout << "B";
-                }
-                einsDavor = false;
-                break;
-            default:
-                if (einsDavor) {
-                    writeValuesToOtherB15('1');
-                    //cout << "1";
-                }
-                writeValuesToOtherB15(ch);
-                //cout << ch;
-                einsDavor = false;
+    }
+
+    else {
+        /* !!! Könnte man bestimmt mit einem int ersetzen !!! */
+        if (p5 == 1) { // Anfang vom Block -> Wert ignorieren und ab jetzt "aufpassen"
+
+        } else if (p4 == 1) { // Ende vom Block -> jetzt kommt die Prüfsumme
+
         }
     }
 }
 
-
-
-void loop() {
-    int s2 = digitalRead(2);
-    int s3 = digitalRead(3);
-    int s4 = digitalRead(4);
-    int s5 = digitalRead(5);
-    int s6 = digitalRead(6);
-    int s7 = digitalRead(7);
-    int s8 = digitalRead(8);
-    int s9 = digitalRead(9);
-
-    neuerWert = (1<<s6) | (2<<s7) | (3<<s8) | (4<<s9);
-
-    /*Serial.print("alter Wert: ");
-    Serial.print(alterWert, BIN);*/
-
-    if (neuerWert != alterWert) {
-        Serial.print("Counter: ");
-        Serial.print(counter);
-        Serial.print(" - ");
-        alterWert = neuerWert;
-        Serial.print(s2);
-        Serial.print(s3);
-        Serial.print(s4);
-        Serial.print(s5);
-        Serial.print(" ");
-        Serial.print(s6);
-        Serial.print(s7);
-        Serial.print(s8);
-        Serial.print(s9);
-        Serial.println();
-        counter++;
-    }
-
-    /*Serial.print("\nneuer Wert: ");
-    Serial.print(neuerWert, BIN);
-    Serial.println("\n");*/
+void sendBuffer() {
+    // Den Buffer senden, wenn Prüfsumme richtig
+    // Buffer sollte ein Array.. oder bitset sein?
 }
-
