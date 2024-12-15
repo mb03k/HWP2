fd7c26de91ebc08de30e3ddee63e1e591bba3e31
matthes <matthesboettcher03@gmail.com>
2024-12-15 16:43:57 +0100

    Main startet die Programme in Threads - fullduplex sollte hiermit moeglich sein

--- HWP2/Threads/B15/main.cpp
+++ HWP2/Threads/B15/main.cpp
@@ -1,5 +1,15 @@
-//
-// Created by Matthes Böttcher on 15.12.24.
-//
+#include <iostream>
+#include <thread>
 
 #include "main.h"
+#include "senden.h"
+#include "empfaengen.h"
+
+int main() {
+    std::thread thread1(init_reading);
+    std::thread thread2(init_sending);
+
+    thread1.join();
+    thread2.join();
+    return 0;
+}
